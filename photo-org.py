75870c87beebe42eda4e48da183f1aa8ba63734a
b-d-curry <b.d.curry@gmail.com>
2021-02-12 21:26:00 +0000

    Update photo-org.py

--- photo-org.py
+++ photo-org.py
@@ -1,5 +1,6 @@
+""" Iterate over the files in "To Be Sorted" and 
+"""
 import os, time, shutil
-
 
 topdir = '/volume1/media/Pictures/To Be Sorted/'
 destdir = '/volume1/media/Pictures/Sorted'
@@ -12,7 +13,7 @@ for dirpath, dirnames, files in os.walk(topdir):
         if name.casefold() == 'Thumbs.db'.casefold() or name.casefold() == '.picasa'.casefold() or name.casefold() == '.picasa.ini'.casefold():
             continue
         # Now identify the filename (used to decide if video or not)
-        filename        = os.path.join(dirpath, name)
+        filename       = os.path.join(dirpath, name)
         (basename,ext) = os.path.splitext(name)
         # Calculate the timestamp, we'll use the older of creation vs modified
         fctime    = os.path.getctime(filename)
@@ -21,11 +22,10 @@ for dirpath, dirnames, files in os.walk(topdir):
         timestamp = time.ctime(ftime)
         # Get the filesize to calculate duplicates
         fsize     = os.path.getsize(filename)
-        # Now create a naming convention for the destination
+        # Now create a naming convention for the destination (YEAR/YEAR - MONTH)
         folder    = time.strftime("%Y", time.localtime(ftime))
         subfolder = time.strftime("%Y - %b", time.localtime(ftime))
         destpath  = os.path.join(destdir, folder, subfolder, name)
-        linkpath  = os.path.join(destdir, 'Videos', folder, subfolder, name)
         # If the destination already exists and is of the same file size, then skip it
         if (os.path.exists(destpath) and fsize == os.path.getsize(destpath)):
             print("Skipping duplicate ",filename)
@@ -41,10 +41,3 @@ for dirpath, dirnames, files in os.walk(topdir):
         os.makedirs(os.path.dirname(destpath), exist_ok=True)
         print("Copying to: " + destpath)
         shutil.copy2(filename, destpath)
-        # If its a Video file, then also create a symbolic link in the Videos dir tree
-        #if ext.casefold() == '.MTS'.casefold() or ext.casefold() == '.MOV'.casefold() or ext.casefold() == '.MP4'.casefold() or ext.casefold() == '.MPG' or ext.casefold() == '.M4V':
-            #os.makedirs(os.path.dirname(linkpath), exist_ok=True)
-            #print("Making a link: " + linkpath)
-            #os.symlink(destpath, linkpath)
-
-
